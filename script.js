4d4bedb060bf2a972df087ce7527ec73a525203b
meaghangallant <hello@meaghangallant.com>
2023-05-31 10:02:02 -0400

    Function #2 takes user choice + returns a winner

--- script.js
+++ script.js
@@ -8,32 +8,25 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 //FUNCTION #2: Play a round of the game
 function playRound(playerSelection, computerSelection) {
-  playerSelection = choose;
   computerSelection = getComputerChoice();
 
   if (playerSelection == computerSelection) {
-    return "It's a tie!";
-  } else if (playerSelection == "rock" && computerSelection == "scissors" 
-    || playerSelection == "paper" && computerSelection == "rock" 
-    || playerSelection == "scissors" && computerSelection == "rock") {
-      return 'You win!!';
-    } else {
-      return "Sorry, you lose";
-    }
-
-
-  /*
-  Rock beats scissors
-  Paper beats rock
-  Scissors beats rock 
-  */
+      return `You both chose ${playerSelection}. It's a tie!`;
+  } else if (playerSelection == "rock" && computerSelection == "scissors"
+      || playerSelection == "paper" && computerSelection == "rock"
+      || playerSelection == "scissors" && computerSelection == "rock") {
+      return `${playerSelection} beats ${computerSelection}, You win!!`;
+  } else {
+      return `${computerSelection} beats ${playerSelection}. Sorry, you lose.`;
+  }
 }
 
-console.log(playRound(rock));
+console.log(playRound("rock"));
+
 
 //Function #3: Play a game (5 rounds)
 //function playGame(){}
