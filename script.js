c8e71c5aae3beeb25808f451404fba506b1d6c59
meaghangallant <hello@meaghangallant.com>
2023-05-31 10:45:37 -0400

    Func #3 plays 5 rounds + returns winner

--- script.js
+++ script.js
@@ -7,23 +7,44 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getPlayerSelection(){
+function getPlayerSelection() {
   let playerChoice = prompt("Will you choose rock, paper, or scissors?");
 
   return playerChoice;
 }
 
 //FUNCTION #2: Play a round of the game
-function playRound(){
-  playerSelection = getPlayerSelection();
-  computerSelection = getComputerChoice();
+function playRound() {
+  let playerSelection = getPlayerSelection();
+  let computerSelection = getComputerChoice();
   if (playerSelection == computerSelection) {
-      return `You both chose ${playerSelection}. It's a tie!`;
+      console.log(`You both chose ${playerSelection}. It's a tie!`);
+      return 0;
   } else if (playerSelection == "rock" && computerSelection == "scissors"
       || playerSelection == "paper" && computerSelection == "rock"
       || playerSelection == "scissors" && computerSelection == "rock") {
-      return `${playerSelection} beats ${computerSelection}, You win!!`;
+      console.log(`${playerSelection} beats ${computerSelection}, You win!!`);
+      return 1;
   } else {
-      return `${computerSelection} beats ${playerSelection}. Sorry, you lose.`;
+      console.log(`${computerSelection} beats ${playerSelection}. Sorry, you lose.`)
+      return -1;
   }
 }
+
+//Function #3: Play a gam,e (5 rounds)
+function playGame() {
+  let winTally = 0;
+  for (let i = 0; i < 5; i++) {
+      let round = playRound();
+      winTally = winTally + round;
+  }
+  if(winTally > 0) {
+      return "You won!!";
+  } else if (winTally < 0) {
+      return "Sorry, you lost";
+  } else {
+      return "It was a tie!"
+  }
+}
+
+//console.log(playGame())
